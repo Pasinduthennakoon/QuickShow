9255f227b18cfc2846bd9463f5f9ddfa71717457
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2026-07-30 23:21:03 +0530

    create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import { Route, Routes, useLocation } from 'react-router'
+import Home from './pages/Home'
+import SeatLayout from './Pages/SeatLayout'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import MyBookings from './pages/MyBookings'
+import Favorite from './pages/Favorite'
+import Footer from './Components/Footer/Footer'
+import { Toaster } from 'react-hot-toast'
+
 
 function App() {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin');
+
   return (
-    <div>App</div>
+    <>
+      <Toaster />
+      { !isAdminRoute && <NavBar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favorite' element={<Favorite />} />
+      </Routes>
+      { !isAdminRoute && <Footer />}
+    </>
   )
 }
 
